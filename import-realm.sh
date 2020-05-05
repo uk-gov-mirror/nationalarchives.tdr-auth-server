c200ccabcc5e0c074a97da34b81ff31fd7a3a87b
TomJKing <tom.king@nationalarchives.gov.uk>
2020-05-05 13:17:31 +0100

    Add backend checks client secret to realm json

--- import-realm.sh
+++ import-realm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cat /tmp/realm-export.json | jq '(.clients[] | select (.clientId == "tdr").secret) = env.CLIENT_SECRET' > /tmp/realm.json
+cat /tmp/realm-export.json | jq '(.clients[] | select (.clientId == "tdr").secret) = env.CLIENT_SECRET' \
+       | jq '(.clients[] | select (.clientId == "tdr-backend-checks").secret) = env.BACKEND_CHECKS_CLIENT_SECRET' > /tmp/realm.json
 sed -i "s,http://localhost:9000,$FRONTEND_URL,g" /tmp/realm.json
 
 /opt/jboss/tools/docker-entrypoint.sh
