f2f15157c5d3f507d1215fce90978bb4edbb36ec
TomJKing <tom.king@nationalarchives.gov.uk>
2020-05-07 15:31:46 +0100

    Roll back env variable name change

--- import-realm.sh
+++ import-realm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat /tmp/realm-export.json | jq '(.clients[] | select (.clientId == "tdr").secret) = env.FRONTEND_APP_CLIENT_SECRET' \
+cat /tmp/realm-export.json | jq '(.clients[] | select (.clientId == "tdr").secret) = env.CLIENT_SECRET' \
        | jq '(.clients[] | select (.clientId == "tdr-backend-checks").secret) = env.BACKEND_CHECKS_CLIENT_SECRET' > /tmp/realm.json
 sed -i "s,http://localhost:9000,$FRONTEND_URL,g" /tmp/realm.json
 
