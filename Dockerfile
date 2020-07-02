ebf1a09ec90be4031a874a2ed158a584dd07964f
TomJKing <tom.king@nationalarchives.gov.uk>
2020-07-02 09:16:43 +0100

    Add property json for TDR environments
    Individual property json files for each TDR environment to provide specific configurations for each environment if required
    
    Python script to set Keycloak realm configuration for TDR environment. Python used as rules for configuration setting getting complex. Script reads environment properties and property json files to set the correct Keycloak realm configuration for the given TDR environment
    
    Configuration json for each Keycloak realm combined into single json configuration file which is imported on Docker container start up

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,17 @@ COPY tdr-entrypoint.sh /opt/jboss
 USER root
 RUN curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > /usr/local/bin/jq \
     && chmod +x /usr/local/bin/jq
+RUN microdnf update && microdnf install python3
 RUN chown jboss /opt/jboss/tdr-entrypoint.sh && chmod +x /opt/jboss/tdr-entrypoint.sh
-USER 1000
+COPY environment-properties /tmp/environment-properties
+COPY update-env-realm-configuration.py /tmp
 COPY master-realm-export.json /tmp
 COPY tdr-realm-export.json /tmp
 COPY import-realm.sh /tmp
 COPY standalone-ha.xml /opt/jboss/keycloak/standalone/configuration/
 COPY govuk/ /opt/jboss/keycloak/themes/govuk/
+RUN chown jboss /tmp/environment-properties/intg_properties.json && chown jboss /tmp/environment-properties/staging_properties.json
+RUN chown jboss /tmp/master-realm-export.json && chown jboss /tmp/tdr-realm-export.json
+USER 1000
+
 ENTRYPOINT /tmp/import-realm.sh
--- import-realm.sh
+++ import-realm.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# retrieves secret values for the TDR realm and replaces placeholder secret values
+# retrieves secret values for each TDR realm client and replaces placeholder secret values
+# adds environment specific properties to each TDR realm client
 # creates a json file containing both top level realm and TDR realm configuration in an array
 # allows all TDR realms to be imported as single json configuration file
 
-cat /tmp/tdr-realm-export.json | jq '(.clients[] | select (.clientId == "tdr").secret) = env.CLIENT_SECRET' \
-      | jq '(.clients[] | select (.clientId == "tdr-backend-checks").secret) = env.BACKEND_CHECKS_CLIENT_SECRET' > /tmp/tdr-realm.json
-sed -i "s,http://localhost:9000,$FRONTEND_URL,g" /tmp/tdr-realm.json
+python3 /tmp/update-env-realm-configuration.py
 
+#combine to single realm configuration json
 {
   echo '['
-  cat /tmp/master-realm-export.json
+  cat /tmp/master-realm.json
   echo ','
   cat /tmp/tdr-realm.json
   echo ']'
 } >> /tmp/realm.json
 
 /opt/jboss/tdr-entrypoint.sh
+
+#/opt/jboss/tools/docker-entrypoint.sh
