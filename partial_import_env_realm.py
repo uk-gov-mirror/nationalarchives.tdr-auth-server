eb197130dd4a7de74d061281e736c4675e8011d0
TomJKing <tom.king@nationalarchives.gov.uk>
2020-07-27 15:05:35 +0100

    Add update configuration stages to Jenkins pipeline

--- partial_import_env_realm.py
+++ partial_import_env_realm.py
@@ -1,19 +1,17 @@
 import requests
 import json
-import os
 import sys
 
 from update_keycloak_configuration import update_keycloak_configuration
 
 stage_to_url = dict(
-    local='http://localhost:8081',
     intg='https://auth.tdr-integration.nationalarchives.gov.uk/',
     staging='https://auth.tdr-staging.nationalarchives.gov.uk/'
 )
 
 stage: str = sys.argv[1]
-keycloak_user = os.environ['KEYCLOAK_USER']
-keycloak_password = os.environ['KEYCLOAK_PASSWORD']
+keycloak_user: str = sys.argv[2]
+keycloak_password: str = sys.argv[3]
 base_auth_url = stage_to_url.get(stage)
 keycloak_auth_url = f'{base_auth_url}/auth/realms/master/protocol/openid-connect/token'
 keycloak_tdr_partial_import_url = f'{base_auth_url}/auth/admin/realms/tdr/partialImport'
@@ -26,7 +24,7 @@ def get_access_token():
     return access_token
 
 
-update_keycloak_configuration('/tmp/')
+update_keycloak_configuration('')
 token = get_access_token()
 headers = {'Content-Type': 'application/json', 'Authorization': f'bearer {token}'}
 
@@ -34,5 +32,3 @@ with open('/tmp/tdr-realm.json', 'r+') as tdr_realm:
     tdr_realm_data = json.load(tdr_realm)
 
 requests.post(keycloak_tdr_partial_import_url, data=json.dumps(tdr_realm_data), headers=headers)
-
-
