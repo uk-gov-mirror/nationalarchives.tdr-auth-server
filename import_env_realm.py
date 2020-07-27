2ad0d2ce985aa826db73c23c61b76dade4011d13
TomJKing <tom.king@nationalarchives.gov.uk>
2020-07-27 13:18:17 +0100

    Initial commit

--- import_env_realm.py
+++ import_env_realm.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
 import subprocess
-import json
-import os
 
-from update_env_client_configuration import update_client_configuration
+from update_keycloak_configuration import update_keycloak_configuration
 
-env_properties = os.environ['KEYCLOAK_CONFIGURATION_PROPERTIES']
-
-with open('/tmp/tdr-realm-export.json', 'r+') as tdr_realm:
-    tdr_realm_data = json.load(tdr_realm)
-with open(f'/tmp/environment-properties/{env_properties}', 'r+') as env_props:
-    env_props_data = json.load(env_props)
-
-    tdr_clients = tdr_realm_data['clients']
-    tdr_client_props = env_props_data['clients']
-
-    update_client_configuration(tdr_clients, tdr_client_props)
-
-with open('/tmp/tdr-realm.json', 'w') as tdr_realm:
-    json.dump(tdr_realm_data, tdr_realm)
-
+update_keycloak_configuration('/tmp/')
 subprocess.call(['/opt/jboss/tools/docker-entrypoint.sh'])
---
+++ partial_import_env_realm.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import os
+import sys
+
+from update_keycloak_configuration import update_keycloak_configuration
+
+stage_to_url = dict(
+    local='http://localhost:8081',
+    intg='https://auth.tdr-integration.nationalarchives.gov.uk/',
+    staging='https://auth.tdr-staging.nationalarchives.gov.uk/'
+)
+
+stage: str = sys.argv[1]
+keycloak_user = os.environ['KEYCLOAK_USER']
+keycloak_password = os.environ['KEYCLOAK_PASSWORD']
+base_auth_url = stage_to_url.get(stage)
+keycloak_auth_url = f'{base_auth_url}/auth/realms/master/protocol/openid-connect/token'
+keycloak_tdr_partial_import_url = f'{base_auth_url}/auth/admin/realms/tdr/partialImport'
+
+def get_access_token():
+    payload = {'username': keycloak_user, 'password': keycloak_password, 'grant_type': 'password', 'client_id': 'admin-cli'}
+    authentication_request = requests.post(keycloak_auth_url, data=payload)
+    authentication_data = authentication_request.json()
+    access_token = authentication_data['access_token']
+    return access_token
+
+
+update_keycloak_configuration('/tmp/')
+token = get_access_token()
+headers = {'Content-Type': 'application/json', 'Authorization': f'bearer {token}'}
+
+with open('/tmp/tdr-realm.json', 'r+') as tdr_realm:
+    tdr_realm_data = json.load(tdr_realm)
+
+requests.post(keycloak_tdr_partial_import_url, data=json.dumps(tdr_realm_data), headers=headers)
+
+
