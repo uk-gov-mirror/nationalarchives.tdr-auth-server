f7b5f68079d6174b5598b1ae4cc5c734ce9a9b91
TomJKing <tom.king@nationalarchives.gov.uk>
2020-07-31 09:56:58 +0100

    Add local properties

--- update_env_realm.py
+++ update_env_realm.py
@@ -74,7 +74,7 @@ def update_realm():
         data=json.dumps(full_realm_data),
         headers=headers
     )
-    print(f"Top Level Result: {top_level_result.text}")
+    print(f"Top Level Result: {top_level_result.status_code}")
 
     partial_result = requests.post(
         f'{base_auth_url}/auth/admin/realms/tdr/partialImport',
