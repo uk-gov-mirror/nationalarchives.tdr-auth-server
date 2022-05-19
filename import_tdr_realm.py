7f0fdc04e9c6e3279bb72afc5f7e8c6a010c91ff
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-05-19 09:49:26 +0100

    Set json output and enable jboss logging

--- import_tdr_realm.py
+++ import_tdr_realm.py
@@ -8,4 +8,4 @@ env_properties_file = os.environ['KEYCLOAK_CONFIGURATION_PROPERTIES']
 host = os.environ['KEYCLOAK_HOST']
 
 update_realm_configuration('/tmp/', env_properties_file)
-subprocess.call(['/opt/keycloak/bin/kc.sh', 'start', '--hostname', host, '--proxy', 'edge'])
+subprocess.call(['/opt/keycloak/bin/kc.sh', 'start', '--hostname', host, '--proxy', 'edge', '--log-console-output', 'json'])
