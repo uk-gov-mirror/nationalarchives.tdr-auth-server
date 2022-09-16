222e29f3a06d084a39e7a84101e28626352fbbff
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-09-16 08:23:43 +0000

    Update circe-core, circe-generic, ... to 0.14.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val keycloakVersion = "19.0.1"
-  private val circeVersion = "0.14.2"
+  private val circeVersion = "0.14.3"
 
   lazy val awsSecretsManager = "com.amazonaws.secretsmanager" % "aws-secretsmanager-caching-java" % "1.0.2"
   lazy val awsUtils: ModuleID = "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.27"
