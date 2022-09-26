03fea7ed8bf99133e13f26f00abd686893f43601
techncl <nigel.lutale@nationalarchives.gov.uk>
2022-09-26 11:18:05 +0100

    Move the 'nvm install' from the script to the README

--- local_setup.sh
+++ local_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 curl -o tdr-realm-export.json https://github.com/nationalarchives/tdr-configurations/blob/master/keycloak/tdr-realm-export.json &&
-nvm install 16.17.0 &&
 npm install &&
 npm run build-theme && # This will compile the theme sass, copy the static assets to the theme `resource` directory and compile the typescript for WebAuthn.
 sbt assemblyPackageDependency assembly # This will generate the jar for the GovUK Notify service and the Event Publisher service
