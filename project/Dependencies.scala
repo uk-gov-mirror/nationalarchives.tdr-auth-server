7558743588d11bf5f59b314d6b205d95e70ebbf2
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-12-01 08:24:03 +0000

    Update quarkus-credentials to 2.15.0.CR1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   lazy val keycloakServerSpi: ModuleID = "org.keycloak" % "keycloak-server-spi" % keycloakVersion % "provided"
   lazy val mockito: ModuleID = "org.mockito" %% "mockito-scala" % "1.17.12"
   lazy val notifyJavaClient: ModuleID = "uk.gov.service.notify" % "notifications-java-client" % "3.19.0-RELEASE"
-  lazy val quarkusCredentials = "io.quarkus" % "quarkus-credentials" % "2.14.2.Final"
+  lazy val quarkusCredentials = "io.quarkus" % "quarkus-credentials" % "2.15.0.CR1"
   lazy val rds = "software.amazon.awssdk" % "rds" % "2.18.11"
   lazy val scalaCache = "com.github.cb372" %% "scalacache-caffeine" % "0.28.0"
   lazy val scalaTest: ModuleID = "org.scalatest" %% "scalatest" % "3.2.14"
