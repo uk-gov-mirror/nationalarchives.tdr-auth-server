2b25384364e0484027c2fb7c6526a290fbcc08e7
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-05-16 08:22:37 +0000

    Update scalatest to 3.2.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   lazy val quarkusCredentials = "io.quarkus" % "quarkus-credentials" % "3.0.3.Final"
   lazy val rds = "software.amazon.awssdk" % "rds" % "2.20.1"
   lazy val scalaCache = "com.github.cb372" %% "scalacache-caffeine" % "0.28.0"
-  lazy val scalaTest: ModuleID = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val scalaTest: ModuleID = "org.scalatest" %% "scalatest" % "3.2.16"
   lazy val snsSdk = "software.amazon.awssdk" % "sns" % "2.20.1"
   lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.20.1"
   lazy val typeSafeConfig = "com.typesafe" % "config" % "1.4.2"
