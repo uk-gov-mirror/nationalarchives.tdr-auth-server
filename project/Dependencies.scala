ae433c2031ae9fc3a57ada4081efe9318ab599b9
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-09-13 08:23:34 +0000

    Update keycloak-core, keycloak-model-jpa, ... to 22.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val keycloakVersion = "22.0.1"
+  private val keycloakVersion = "22.0.3"
   private val circeVersion = "0.14.6"
 
   lazy val awsSecretsManager = "com.amazonaws.secretsmanager" % "aws-secretsmanager-caching-java" % "2.0.0"
