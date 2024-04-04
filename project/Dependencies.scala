d01a5c1e191ac43b0f9801b6094bc97f8c05b69b
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-04-04 08:22:12 +0000

    Update mockito-scala to 1.17.31 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   lazy val keycloakCore: ModuleID = "org.keycloak" % "keycloak-core" % keycloakVersion % "provided"
   lazy val keycloakModelJpa: ModuleID = "org.keycloak" % "keycloak-model-jpa" % keycloakVersion % "provided"
   lazy val keycloakServerSpi: ModuleID = "org.keycloak" % "keycloak-server-spi" % keycloakVersion % "provided"
-  lazy val mockito: ModuleID = "org.mockito" %% "mockito-scala" % "1.17.30"
+  lazy val mockito: ModuleID = "org.mockito" %% "mockito-scala" % "1.17.31"
   lazy val notifyJavaClient: ModuleID = "uk.gov.service.notify" % "notifications-java-client" % "5.0.0-RELEASE"
   lazy val quarkusCredentials = "io.quarkus" % "quarkus-credentials" % "3.9.1"
   lazy val rds = "software.amazon.awssdk" % "rds" % "2.25.21"
