fc69c9b7eaf51666832285b73873fb6e4d5e07c0
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-04-10 08:22:55 +0000

    Update notifications-java-client to 5.0.1-RELEASE in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   lazy val keycloakModelJpa: ModuleID = "org.keycloak" % "keycloak-model-jpa" % keycloakVersion % "provided"
   lazy val keycloakServerSpi: ModuleID = "org.keycloak" % "keycloak-server-spi" % keycloakVersion % "provided"
   lazy val mockito: ModuleID = "org.mockito" %% "mockito-scala" % "1.17.31"
-  lazy val notifyJavaClient: ModuleID = "uk.gov.service.notify" % "notifications-java-client" % "5.0.0-RELEASE"
+  lazy val notifyJavaClient: ModuleID = "uk.gov.service.notify" % "notifications-java-client" % "5.0.1-RELEASE"
   lazy val quarkusCredentials = "io.quarkus" % "quarkus-credentials" % "3.9.2"
   lazy val rds = "software.amazon.awssdk" % "rds" % "2.25.26"
   lazy val scalaCache = "com.github.cb372" %% "scalacache-caffeine" % "0.28.0"
